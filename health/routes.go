57595d63796912d8920bfd56976782ea99d9faf2
Hendro Prabowo <awebened@gmail.com>
2023-09-24 12:25:21 +0700

    refactor: add no path endpoint

--- health/routes.go
+++ health/routes.go
@@ -1,12 +1,15 @@
 package health
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/hellofresh/health-go/v5"
 	"github.com/newrelic/go-agent/v3/newrelic"
 
 	"lending-service/config/monitoring"
 	"lending-service/constant"
+	"lending-service/utility/response"
 )
 
 func RegisterRoutes(r *chi.Mux) {
@@ -15,5 +18,11 @@ func RegisterRoutes(r *chi.Mux) {
 		Version: "v1.0",
 	}))
 
+	r.Get(newrelic.WrapHandleFunc(monitoring.NewrelicApp, "/", func(w http.ResponseWriter, r *http.Request) {
+		resp := map[string]interface{}{
+			"app_name": "lending-service",
+		}
+		response.Ok(w, resp)
+	}))
 	r.Get(newrelic.WrapHandleFunc(monitoring.NewrelicApp, "/status", h.HandlerFunc))
 }
